65896399669c325eef4cf6c5367a2fa779259944
Leonardo Araújo <progamadorweb18@gmail.com>
2025-08-22 13:22:40 -0300

    Adicionando Tarefas

--- js/script.js
+++ js/script.js
@@ -1,2 +1,39 @@
 let input = document.getElementById("inputTarefa");
 let btnAdd = document.getElementById("btn-add");
+let main = document.getElementById("areaLista");
+
+function addTarefa(){
+    // PEGAR VALOR DO INPUT
+    let valorInput = input.value;
+
+    if((valorInput !== "") && (valorInput !== null) && (valorInput !== undefined)){
+        let novoItem = `
+            <div class="item">
+            <div class="item-icone">
+                <i class="mdi mdi-circle-outline"></i>
+            </div>
+            <div class="item-nome">
+                ${valorInput}
+            </div>
+            <div class="item-botao">
+                <button onclick="deletar()" class="delete" ><i class="mdi mdi-delete"></i> Deletar</button>
+            </div>
+        </div>
+        `;
+
+        // ADICIONAR NOVO ITEM NO MAIN
+        main.innerHTML += novoItem;
+
+        // ZERAR E FOCAR CAMPO
+        input.value = "";
+        input.focus();
+    }
+}
+
+input.addEventListener("keyup", function(event){
+    // SE TECLOU ENTER (13)
+    if(event.keyCode === 13){
+        event.preventDefault();
+        btnAdd.click();
+    }
+})
